232547ceb61f4dc917fe893c24df1214adf32b76
manu-cj <carenjotje@gmail.com>
2024-06-17 14:36:47 +0200

    add weatherapp exercice

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="https://kit.fontawesome.com/d8438e7f2f.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="/weatherApp/">weatherApp</a></li>
+        <li><a href="/exos/php-challenge-auth/">auth</a></li>
+        <li><a href="/exos/php-exercises-crud1/">crud1</a></li>
+        <ul>
+            <li><li><a href="/exos/php-exercises-crud2//Exercice_7/">crud-exo 7</a></li></li>
+            <li><li><a href="/exos/php-exercises-crud2//Exercice_8/">crud-exo 8</a></li></li>
+            <li><li><a href="/exos/php-exercises-crud2//Exercice_9/">crud-exo 9</a></li></li>
+        </ul>
+        <li><a href="/exos/php-training-mysql/">training-mysql</a></li>
+        <li><a href="/sql-exo/">sql-exo</a></li>
+    </ul>
+</nav>
+<?php
+session_start();
+?>
